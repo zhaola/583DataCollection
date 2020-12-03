356f3f9b91a8788dce393638b569fd6de1b2c529
Lawrence Zhao <lazhao@umich.edu>
2020-12-03 00:20:04 +0000

    Got timing pass running on the susan file from the mibench dataset

--- timepass/run.sh
+++ timepass/run.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
-set +euxo pipefail
+set -euxo pipefail
 
 LIBPATH="build/timepass/LLVMPJT.so"
 PASSNAME="time"
 HARNESSPATH="time_wrapper/time_wrapper.o"
+OUTDIR="tmp"
+
+makeOutDir() {
+    rm -rf $OUTDIR
+    mkdir -p $OUTDIR
+}
+
+timeProgram() {
+    CFILE="toy"
+    SRCDIR="./"
+    INPUT=""
+    # CFILE="susan"
+    # SRCDIR="/home/lazhao/mibench/automotive/susan"
+    # INPUT="$SRCDIR/input_small.pgm -c"
+
+    makeOutDir
+
+    clang -c -o $OUTDIR/$CFILE.bc -O0 -emit-llvm $SRCDIR/$CFILE.c
+    opt -o $OUTDIR/$CFILE.time.bc -load $LIBPATH -$PASSNAME < $OUTDIR/$CFILE.bc > /dev/null
+
+    clang -o $OUTDIR/${CFILE}_time.out $OUTDIR/$CFILE.time.bc $HARNESSPATH -lm -I$SRCDIR
+    taskset --cpu-list 0 ./$OUTDIR/${CFILE}_time.out $INPUT
+}
 
 ./build.sh
 
-CFILE="toy"
-clang -emit-llvm $CFILE.c -c -o $CFILE.bc -O0
-opt -o $CFILE.time.bc -load $LIBPATH "-$PASSNAME" < $CFILE.bc > /dev/null
-clang $CFILE.time.bc $HARNESSPATH -o ${CFILE}_time.out
-taskset --cpu-list 0 ./${CFILE}_time.out
-rm $CFILE.bc $CFILE.time.bc ${CFILE}_time.out
+timeProgram
